eaafb6bb3462b4b56042a69e0dcad1cb03ade449
yosefus <yosalsoni@gmail.com>
2022-11-18 12:17:44 +0200

    בשרא 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Cart from './Cart';
+import Cart2 from './Cart2';
 import Persons from './Persons';
 import Testing from './Test';
 
 function App() {
   return (
     <div className="App">
-      <Cart />
+      {/* <Cart /> */}
+      <Cart2 />
       {/* <Testing /> */}
       {/* <Persons /> */}
     </div>
